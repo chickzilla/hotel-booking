b290fa11911b3f2ede3e1e6720d6654683ad3bd4
Dulyawat Khamkaew <6430127621@student.chula.ac.th>
2023-10-11 18:22:00 +0700

    fix : File position

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-import Banner from "./components/Banner";
+import Banner from "@/components/Banner";
 
 export default function Home() {
   return (
