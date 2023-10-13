f4e7a2100844c462feb156418b8353c63e85f9ba
Dulyawat Khamkaew <6430127621@student.chula.ac.th>
2023-10-13 19:38:51 +0700

    fix : Banner
    delete picture and + weight <p>

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -4,10 +4,10 @@ import Image from "next/image";
 import { useState } from "react";
 
 export default function Banner() {
-  const covers = ["/img/Banner8.png", "/img/Banner5.jpg"];
+  const covers = ["/img/Banner8.png", "/img/Banner1.png"];
 
   const [index, setIndex] = useState(0);
-  const isOddIndex = index % 2 == 1;
+
   return (
     <div
       className="block p-[5px] m-[0px] w-auto h-[75vh] relative bg-white"
@@ -23,18 +23,14 @@ export default function Banner() {
         priority
         sizes="500vh"
       ></Image>
-      <div
-        className={`relative ${
-          isOddIndex ? "text-black" : "text-white"
-        } left-[5%] top-[15%] w-[40%] font-sans`}
-      >
+      <div className={"relative left-[5%] top-[12%] w-[40%] font-sans"}>
         <div className="text-[5em] mb-[40px] leading-[95px] text-7xl text-black font-bold  text-transparent bg-clip-text bg-gradient-to-r from-zinc-400 to-zinc-700">
           Explore your place to stay
         </div>
         <p className="mt-[20px] text-zinc-700 mb-[10px]">
           ──────────────────────────────────────────────────
         </p>
-        <p className="text-transparent bg-clip-text bg-gradient-to-r from-zinc-600 to-zinc-900 w-[60%]">
+        <p className="text-transparent bg-clip-text bg-gradient-to-r from-zinc-600 to-zinc-900 w-[60%] font-semibold">
           Unlock Unforgettable Adventures, One Reservation at a Time: Your
           Trusted Partner in Crafting Dreamy Escapes, Exceptional Comfort, and
           Lasting Memories.
