1a57c2d6f83958f7d9f8121baf1b9d5698fbfa2f
Dulyawat Khamkaew <6430127621@student.chula.ac.th>
2023-11-12 14:42:02 +0700

    DELETE TEXT ADD HOTEL

--- src/components/HotelAddForm.tsx
+++ src/components/HotelAddForm.tsx
@@ -1,83 +1,143 @@
 "use client";
 
-import { useState } from "react";
 import TextFieldAddHotel from "./TextFieldAddHotel";
 import HotelIcon from "@mui/icons-material/Hotel";
 import ImportContactsIcon from "@mui/icons-material/ImportContacts";
 import LocalPostOfficeIcon from "@mui/icons-material/LocalPostOffice";
 import LocalPhoneIcon from "@mui/icons-material/LocalPhone";
 import AddAPhotoIcon from "@mui/icons-material/AddAPhoto";
+import { TextField } from "@mui/material";
+import InputAdornment from "@mui/material/InputAdornment";
 
 export default function HotelAddForm() {
-  const [name, setName] = useState("");
-  const [addess, setAddress] = useState("");
-  const [district, setDistrict] = useState("");
-  const [province, setProvince] = useState("");
-  const [postalCode, setPostalCode] = useState("");
-  const [tel, setTel] = useState("");
-  const [picture, setPicture] = useState("");
-
   return (
     <div className="flex flex-col justify-center border drop-shadow-xl px-5 py-10 w-[35%] space-y-12 items-center bg-white">
       <div className="text-2xl font-semibold w-[100%] px-5">Hotel Detail</div>
 
-      <TextFieldAddHotel
-        setParameter={(value: string) => {
-          setName(value);
+      <TextField
+        id="hotel name"
+        label="hotel name"
+        name="hotel name"
+        variant="outlined"
+        sx={{
+          width: "90%",
         }}
-        header="Hotel name"
-        icon={<HotelIcon />}
-      />
-      <TextFieldAddHotel
-        setParameter={(value: string) => {
-          setAddress(value);
+        size="small"
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <HotelIcon />
+            </InputAdornment>
+          ),
         }}
-        header="Address"
-        icon={<ImportContactsIcon />}
-      />
-
-      <TextFieldAddHotel
-        setParameter={(value: string) => {
-          setDistrict(value);
-        }}
-        header="District"
-        icon={<ImportContactsIcon />}
-      />
-      <TextFieldAddHotel
-        setParameter={(value: string) => {
-          setProvince(value);
-        }}
-        header="Province"
-        icon={<ImportContactsIcon />}
       />
 
-      <TextFieldAddHotel
-        setParameter={(value: string) => {
-          setPostalCode(value);
+      <TextField
+        id="address"
+        label="address"
+        name="address"
+        variant="outlined"
+        sx={{
+          width: "90%",
         }}
-        header="Postal Code"
-        icon={<LocalPostOfficeIcon />}
+        size="small"
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <ImportContactsIcon />
+            </InputAdornment>
+          ),
+        }}
       />
-      <TextFieldAddHotel
-        setParameter={(value: string) => {
-          setTel(value);
+      <TextField
+        id="district"
+        label="district"
+        name="district"
+        variant="outlined"
+        sx={{
+          width: "90%",
         }}
-        header="Tel"
-        icon={<LocalPhoneIcon />}
+        size="small"
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <ImportContactsIcon />
+            </InputAdornment>
+          ),
+        }}
+      />
+      <TextField
+        id="province"
+        label="province"
+        name="province"
+        variant="outlined"
+        sx={{
+          width: "90%",
+        }}
+        size="small"
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <ImportContactsIcon />
+            </InputAdornment>
+          ),
+        }}
+      />
+      <TextField
+        id="postal code"
+        label="postal code"
+        name="postal code"
+        variant="outlined"
+        sx={{
+          width: "90%",
+        }}
+        size="small"
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <LocalPostOfficeIcon />
+            </InputAdornment>
+          ),
+        }}
       />
 
-      <TextFieldAddHotel
-        setParameter={(value: string) => {
-          setPicture(value);
+      <TextField
+        id="tel"
+        label="tel"
+        name="tel"
+        variant="outlined"
+        sx={{
+          width: "90%",
         }}
-        header="Picture"
-        icon={<AddAPhotoIcon />}
+        size="small"
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <LocalPhoneIcon />
+            </InputAdornment>
+          ),
+        }}
       />
+      <TextField
+        id="picture"
+        label="picture"
+        name="picture"
+        variant="outlined"
+        sx={{
+          width: "90%",
+        }}
+        size="small"
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <AddAPhotoIcon />
+            </InputAdornment>
+          ),
+        }}
+      />
+
       <div className="text-center w-[90%] mb-5">
-        <button
-          className="bg-black text-slate-100 py-2 px-3 mb-2 font-semibold rounded z-30 w-24 w-[100%] hover:bg-slate-700 hover:text-white"
-          onClick={() => {}}
-        >
+        <button className="bg-black text-slate-100 py-2 px-3 mb-2 font-semibold rounded z-30 w-24 w-[100%] hover:bg-slate-700 hover:text-white">
           ADD HOTEL
         </button>
         <div className="mt-2 text-zinc-700">
--- src/libs/getAllHotels.tsx
+++ src/libs/getAllHotels.tsx
@@ -2,7 +2,9 @@ export default async function getAllHotels() {
   await new Promise((resolve) => {
     setTimeout(resolve, 3000);
   });
-  const response = await fetch("http://localhost:5000/api/v1/hotels");
+  const response = await fetch("http://localhost:5000/api/v1/hotels", {
+    next: { tags: ["hotels"] },
+  });
 
   if (!response.ok) {
     throw new Error("Cannot fetch get all hotels");
