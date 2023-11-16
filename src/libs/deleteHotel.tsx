4a7f7ffe516b3c457318b37af9b2cbbee00af810
Dulyawat Khamkaew <6430127621@student.chula.ac.th>
2023-11-16 18:54:47 +0700

    Create libs deleteHotel.tsx

---
+++ src/libs/deleteHotel.tsx
@@ -0,0 +1,11 @@
+export default async function deleteHotel(token: string, hid: string) {
+  const response = await fetch(`http://localhost:5000/api/v1/hotels/${hid}`, {
+    method: "DELETE",
+    headers: {
+      authorization: `Bearer ${token}`,
+    },
+  });
+  if (!response.ok) throw new Error("Failed to fetch delete hotel");
+
+  return await response.json();
+}
